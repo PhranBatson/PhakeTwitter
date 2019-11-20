2513a0434db50760a628afe6ff4c731ed68163ba
Phran <FrancisOBatson@gmail.com>
2019-11-20 09:29:13 -0500

    landing changed to tweet/index

--- Startup.cs
+++ Startup.cs
@@ -55,7 +55,7 @@ namespace PhakeTwitter
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Tweets}/{action=Index}/{id?}");
             });
         }
     }
