473770b8986651fac65c21f26f84a02c9ac0547e
Phran <FrancisOBatson@gmail.com>
2019-11-20 08:58:43 -0500

    modified display names

--- Models/Tweet.cs
+++ Models/Tweet.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace PhakeTwitter.Models
 {
@@ -10,7 +11,7 @@ namespace PhakeTwitter.Models
         public int ID { get; set; }
         public int UserID { get; set; }
         public string Text { get; set; }
-
+        [Display(Name = "Post Date / Time")]
         public string PostDate { get; set; }
 
         public User User { get; set; }
--- Models/User.cs
+++ Models/User.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace PhakeTwitter.Models
 {
     public class User
     {
         public int ID { get; set; }
+        [Display(Name = "User Name")]
         public string UserName { get; set; }
         public string Password { get; set; }
 
